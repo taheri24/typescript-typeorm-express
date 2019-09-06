ad1c325495aa5633cc893c20e80bd846dc699d27
Majid Taheri(I love optimization) <taheri24@users.noreply.github.com>
2019-09-06 08:21:25 +0430

    add webpack & single js for build-process

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+const {
+    NODE_ENV = 'production',
+} = process.env;
+
+module.exports = {
+    entry: './src/index',
+     externals:[nodeExternals()],
+    mode: NODE_ENV,
+    node: {
+		__filename: true,
+		__dirname: true
+	},
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'server.bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        modules: [
+             './node_modules'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                use: 'source-map-loader'
+            },
+            {
+                test: /\.ts$/,
+                exclude: [/node_modules/],
+                use: 'ts-loader'
+            }
+        ]
+    }
+}
